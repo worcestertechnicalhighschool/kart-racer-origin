49dec939e7c12081e890ec7f7093cf25c47d9af5
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2024-10-31 12:50:44 -0400

    rename

--- scenes/object_scenes/boost_pad.gd
+++ scenes/object_scenes/boost_pad.gd
@@ -1,24 +1,24 @@
 extends StaticBody3D
 
-var camera_node
+var front_camera_parent
 var front_camera
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if "car" in body.name.to_lower():
+	if body is VehicleBody3D:
 		
-		camera_node = body.find_child("Cameras")
-		front_camera = camera_node.find_child("FrontCamera")
+		front_camera_parent = body.find_child("Cameras")
+		front_camera = front_camera_parent.find_child("FrontCamera")
 		
-		if rotation_degrees.x > rotation_degrees.z:
+		if body.rotation_degrees.x > body.rotation_degrees.z:
 			body.apply_central_force(Vector3(100 * sign(rotation_degrees.x), 0, 0)) 
-		elif rotation_degrees.x < rotation_degrees.z:
+		elif body.rotation_degrees.x < body.rotation_degrees.z:
 			body.apply_central_force(Vector3(0, 0, 100 * sign(rotation_degrees.z))) 
 		
 		var fov_out_tween = get_tree().create_tween()
 		fov_out_tween.tween_property(front_camera, "fov", 115, 0.3)
 		
 		var center_of_mass_rotation_tween = get_tree().create_tween()
-		center_of_mass_rotation_tween.tween_property(camera_node, "rotation_degrees", Vector3(10, 0, 0), 0.3)
+		center_of_mass_rotation_tween.tween_property(front_camera_parent, "rotation_degrees", Vector3(10, 0, 0), 0.3)
 		
 		$CameraTimer.start()
 
@@ -27,4 +27,4 @@ func _on_camera_timer_timeout() -> void:
 	fov_in_tween.tween_property(front_camera, "fov", 75, 0.3)
 
 	var center_of_mass_reversion_tween = get_tree().create_tween()
-	center_of_mass_reversion_tween.tween_property(camera_node, "rotation_degrees", Vector3(0, 0, 0), 0.3)
+	center_of_mass_reversion_tween.tween_property(front_camera_parent, "rotation_degrees", Vector3(0, 0, 0), 0.3)
