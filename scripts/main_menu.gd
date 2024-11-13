6c7b4782f6a9abfb6bf6845a7090b10abbefa762
Eduardo <exostar922@gmail.com>
2024-11-13 11:05:07 -0500

    track select screen added

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,9 @@ extends Control
 
 
 func _on_single_player_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/map_scenes/curve_test.tscn")
-
+	get_tree().change_scene_to_file("res://scenes/ui_scenes/menus/track_select.tscn")
+	
+	
 
 func _on_go_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui_scenes/title_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui_scenes/menus/title_screen.tscn")
