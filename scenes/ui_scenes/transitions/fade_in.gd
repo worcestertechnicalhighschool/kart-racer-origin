f709d96151a256762546c004c26922df95eb42f1
Eduardo <exostar922@gmail.com>
2024-12-02 12:56:44 -0500

    fade in and out, organization of scripts folder

--- scenes/ui_scenes/transitions/fade_in.gd
+++ scenes/ui_scenes/transitions/fade_in.gd
@@ -1,21 +1,25 @@
 extends CanvasLayer
 
-signal on_transition_finished
+signal animation_end
 
 @onready var color_rect = $ColorRect
 @onready var animation_player = $AnimationPlayer
 
 func _ready():
 	color_rect.visible = false
-	animation_player.animation_finished.connect(_on_animation_finished)
-	
-func _on_animation_finished(anim_name):
-	if anim_name == "fade_to_black":
-		on_transition_finished.emit()
-		animation_player.play("fade_to_normal")
-	elif anim_name == "fade_to_normal":
-		color_rect.visible = false
-	
-func transition():
+
+func transition_toblack():
 	color_rect.visible = true
 	animation_player.play("fade_to_black")
+	
+func transition_tonormal():
+	color_rect.visible = true
+	animation_player.play("fade_to_normal")
+	
+func transition_titlescreen():
+	color_rect.visible = true
+	animation_player.play("fade_to_normal_titlescreen")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	animation_end.emit()
+	queue_free()
