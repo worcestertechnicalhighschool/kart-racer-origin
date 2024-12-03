1a4032072a268916eead9fbefaa40fd3499fd9e1
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2024-12-03 11:55:17 -0500

    fixed things that aren't working

--- scripts/menus/debugmenu.gd
+++ scripts/menus/debugmenu.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	visible = false
 	
 	$Label.text = str((OS.get_static_memory_usage() / 1000000))
-	var debug_change_currentlap = $SpinBox
+	var debug_change_currentlap = $changelap
