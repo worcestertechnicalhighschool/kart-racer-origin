1f83b03a9deb42a0ee5815791e62e8eb148fdb6c
Eduardo <exostar922@gmail.com>
2024-12-06 09:29:26 -0500

    little thing rq

--- scripts/menus/debugmenu.gd
+++ scripts/menus/debugmenu.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@onready var debug_change_currentlap = $changelap
+@onready var debug_change_enginepwer = $changeenginepower
+@onready var debug_change_trk = $changetrack
+
 func _ready() -> void:
 	visible = false
 	
-	$Label.text = str((OS.get_static_memory_usage() / 1000000))
-	var debug_change_currentlap = $changelap
-	var debug_change_enginepwer = $changeenginepower
-	var debug_change_trk = $changetrack
+	#$Label.text = str((OS.get_static_memory_usage() / 1000000))
+	
+func change_lap():
+	debug_change_currentlap
