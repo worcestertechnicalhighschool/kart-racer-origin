b61e06e8c5d37a4221449996f9f66fbc0d356220
EduardoWebDevelopment <exostar922@gmail.com>
2024-12-17 10:39:00 -0500

    pause menu rework

--- scripts/menus/pause_menu.gd
+++ scripts/menus/pause_menu.gd
@@ -2,7 +2,9 @@ extends Control
 
 
 func _on_resume_pressed() -> void:
-	pass
+	$".".hide()
+	Engine.time_scale = 1
 
 func _on_quit_pressed() -> void:
+	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://scenes/ui_scenes/menus/title_screen.tscn")
