ebca14a0d9d69de4ee20e1c1ac20324c59327b38
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2024-12-17 12:18:47 -0500

    finished animation

--- scripts/obstacles/mushroom.gd
+++ scripts/obstacles/mushroom.gd
@@ -9,17 +9,40 @@ extends RigidBody3D
 var tween_playing = false
 var front_camera_parent
 var front_camera
-var count = 0
 
+func _visibility():
+	visible = not Forcefield.visible
+	
+func _reset():
+	Forcefield.visible = false
+	Forcefield.mesh.material.albedo_color = original_color
+	
+	$Placeholder.start()
+
+func _ready() -> void:
+	Forcefield.visible = true
+	Forcefield.mesh.material.albedo_color = Color(0, 0, 0, 0)
+	
+	var position_tween_up = create_tween()
+	position_tween_up.set_trans(Tween.TRANS_QUAD)
+	position_tween_up.set_ease(Tween.EASE_IN)
+	position_tween_up.parallel().tween_property(
+		self,
+		"position",
+		Vector3(ThrownObjects.position.x, ThrownObjects.position.y + 1.25, ThrownObjects.position.z),
+		0.1
+	)
+	
+	var forcefield_visibility_tween = get_tree().create_tween()
+	forcefield_visibility_tween.tween_property(
+		Forcefield.mesh.material,
+		"albedo_color",
+		original_color,
+		0.1
+	)
 
 func _process(_delta: float) -> void:
 	position = Vector3(ThrownObjects.position.x, ThrownObjects.position.y + 1.25, ThrownObjects.position.z)
-	Forcefield.visible = true
-	
-	if Forcefield.visible:
-		count += 1
-	
-	print(count)
 	
 	if not tween_playing:
 		tween_playing = true
@@ -31,9 +54,9 @@ func _apply_effects():
 	front_camera = front_camera_parent.find_child("FrontCamera")
 	
 	if Car.rotation_degrees.x >= Car.rotation_degrees.z:
-		Car.apply_central_force(Vector3(100 * sign(rotation_degrees.x), 0, 0)) 
+		Car.apply_central_force(Vector3(500000 * sign(rotation_degrees.x), 0, 0)) 
 	elif Car.rotation_degrees.x < Car.rotation_degrees.z:
-		Car.apply_central_force(Vector3(0, 0, 100 * sign(rotation_degrees.z))) 
+		Car.apply_central_force(Vector3(0, 0, 500000 * sign(rotation_degrees.z))) 
 	
 	var fov_out_tween = get_tree().create_tween()
 	fov_out_tween.tween_property(front_camera, "fov", 115, 0.3)
@@ -43,28 +66,17 @@ func _apply_effects():
 	
 	$CameraTimer.start()
 
-func _visibility():
-	visible = not Forcefield.visible
-	
-func reset():
-	_visibility()
-	Forcefield.mesh.material.albedo_color =  original_color
-
 func _on_camera_timer_timeout() -> void:
 	var fov_in_tween = get_tree().create_tween()
-	fov_in_tween.tween_property(front_camera, "fov", 75, 0.3)
-	fov_in_tween.set_parallel()
+	fov_in_tween.parallel().tween_property(front_camera, "fov", 75, 0.3)
 
 	var center_of_mass_reversion_tween = get_tree().create_tween()
-	center_of_mass_reversion_tween.tween_property(front_camera_parent, "rotation_degrees", Vector3(0, 0, 0), 0.3)
-	center_of_mass_reversion_tween.set_parallel()
+	center_of_mass_reversion_tween.parallel().tween_property(front_camera_parent, "rotation_degrees", Vector3(0, 0, 0), 0.3)
 	
 	var forcefield_visibility_tween = get_tree().create_tween()
-	forcefield_visibility_tween.tween_property(Forcefield.mesh.material, "albedo_color", Color(0, 0, 0, 0), 0.3)
-	forcefield_visibility_tween.set_parallel()
-	
-	forcefield_visibility_tween.tween_callback(reset)
-	center_of_mass_reversion_tween.tween_callback(queue_free)
+	forcefield_visibility_tween.parallel().tween_property(Forcefield.mesh.material, "albedo_color", Color(0, 0, 0, 0), 0.3)
+
+	forcefield_visibility_tween.tween_callback(_reset)
 
 func _on_animation_timer_timeout() -> void:
 	var position_tween_down = create_tween()
@@ -78,3 +90,6 @@ func _on_animation_timer_timeout() -> void:
 	)
 	
 	position_tween_down.tween_callback(_visibility)
+
+func _on_placeholder_timeout() -> void:
+	queue_free()
