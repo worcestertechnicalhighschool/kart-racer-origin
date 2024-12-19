ebf74b59273b9d2d4b4abb6b6d5efa9b374503e9
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2024-12-19 12:27:35 -0500

    quick change

--- scripts/ui/fps.gd
+++ scripts/ui/fps.gd
@@ -2,5 +2,5 @@ extends Label
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	set_text("FPS: %d" % Engine.get_frames_per_second())
