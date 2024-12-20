220ae1eb5089cd92f643d02cbe7b644725194b4f
EduardoWebDevelopment <exostar922@gmail.com>
2024-12-20 11:10:12 -0500

    the sound effects update

--- scripts/ui/menus/title_screen.gd
+++ scripts/ui/menus/title_screen.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready() -> void:
 	$FadeIn.transition_titlescreen()
+	#MusicPlayer.play_music_level()
 
 func _on_single_player_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui_scenes/menus/main_menu.tscn")
--- scripts/ui/menus/track_select.gd
+++ scripts/ui/menus/track_select.gd
@@ -1,10 +1,12 @@
 extends Control
 
 func _on_texture_button_pressed() -> void:
+	$AudioStreamPlayer.stop()
 	get_tree().change_scene_to_file("res://scenes/map_scenes/curve_test.tscn")
 
 
 func _on_donut_test_button_pressed() -> void:
+	$AudioStreamPlayer.stop()
 	get_tree().change_scene_to_file("res://assets/tracks/track_designs/donut_test.tscn")
 
 # goes to previous screen, with fade out
