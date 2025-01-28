225d867c615e5723d06881bb07136ea47283b8f4
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2025-01-28 13:40:24 -0500

    clamp merge

--- scripts/car/car.gd
+++ scripts/car/car.gd
@@ -76,8 +76,8 @@ func _physics_process(delta: float) -> void:
 		#br_wheel.wheel_roll_influence = 1
 		#fr_wheel.wheel_roll_influence = 1
 		
-		rotation_degrees.x = clamp(rotation_degrees.x, -25, 25)
-		rotation_degrees.y = clamp(rotation_degrees.y, -25, 25)
+		rotation_degrees.x = clamp(rotation_degrees.x, -10, 10)
+		rotation_degrees.z = clamp(rotation_degrees.z, -10, 10)
 		
 		if SPEED_BOOST:
 			ENGINE_POWER *= 2
