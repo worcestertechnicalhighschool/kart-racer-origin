d09bbfd5fcc7e5dcf0b5dc4e7340cac77d18f4ce
Ruchir <ruchirkafle@gmail.com>
2025-02-15 18:31:46 -0500

    minimap works again

--- scripts/ui/mini_map.gd
+++ scripts/ui/mini_map.gd
@@ -1,12 +1,11 @@
 extends ColorRect
 
-@export var target : NodePath
+@export var player : VehicleBody3D
 @export var camera_distance := 20.0
 
-@onready var player := get_node(target)
 @onready var camera := $SubViewportContainer/SubViewport/Camera3D
 
 func _process(delta: float) -> void:
-	if target:
+	if player:
 		camera.size = camera_distance
 		camera.position = Vector3(player.position.x, camera_distance, player.position.z)
